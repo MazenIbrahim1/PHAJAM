6d047563468d4fb9b8dc7789f063ee6f9db7b610
Amelia Chin <chinamelia08@gmail.com>
2024-10-12 14:24:49 -0400

    add theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Settings from "./components/Settings/Settings";
 import Status from "./components/Status/Status";
 import Home from "./components/Home/Home";
 import Nav from "./components/Navigation/Nav";
+import { ThemeProvider } from "@mui/material/styles";
+import oceanTheme from "./theme"; // Import the theme from the theme.js file
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={oceanTheme}>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,7 +21,7 @@ function App() {
         <Route path="/peers" element={<Peers />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
